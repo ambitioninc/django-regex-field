cbdca39057ab1caeb382bb3c48d2bd407d128747
Wes Kendall <wes.kendall@tryambition.com>
2014-03-10 22:50:16 -0400

    modified the way that tests are executed and how versions are embedded

--- regex_field/__init__.py
+++ regex_field/__init__.py
@@ -1,3 +1,2 @@
-__version__ = '0.1.3'
-
+from .version import __version__
 from .fields import RegexField
--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
-from setuptools import setup, find_packages
-import regex_field
 # import multiprocessing to avoid this bug (http://bugs.python.org/issue15881#msg170215_
 import multiprocessing
 assert multiprocessing
+import re
+from setuptools import setup, find_packages
+
+
+def get_version():
+    """
+    Extracts the version number from the version.py file.
+    """
+    VERSION_FILE = 'regex_field/version.py'
+    mo = re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', open(VERSION_FILE, 'rt').read(), re.M)
+    if mo:
+        return mo.group(1)
+    else:
+        raise RuntimeError('Unable to find version string in {0}.'.format(VERSION_FILE))
 
 
 setup(
     name='django-regex-field',
-    version=regex_field.__version__,
+    version=get_version(),
     description='Store regular expressions in Django models',
     long_description=open('README.md').read(),
     url='http://github.com/ambitioninc/django-regex-field/',
@@ -27,7 +39,8 @@ setup(
     tests_require=[
         'psycopg2',
         'django-nose',
+        'south',
     ],
-    test_suite='run_tests.run_tests',
+    test_suite='runtests.runtests',
     include_package_data=True,
 )
